ef3ed039843b705574cafbac84ae3fc0837258ce
Mantas Norvaiša <mntnorv@gmail.com>
2014-05-08 07:31:28 +0300

    Add ability to specify defaults through hash

--- js/app.js
+++ js/app.js
@@ -1,10 +1,32 @@
+$.fn.filterByData = function(prop, val) {
+  return this.filter(
+    function() { return $(this).data(prop)==val; }
+  );
+};
+
 $(document).ready(function() {
   var radios = $('.table-view.radio');
   var saveBtn = $('#save');
 
+  var options = {};
+  var hash = window.location.hash.substring(1);
+  if (hash) {
+    try {
+      var parsed = JSON.parse(hash);
+      options = parsed;
+    } catch (e) {
+      console.error('Error parsing options: ', hash);
+    }
+  }
+
   radios.each(function (index) {
     var radio = $(this);
     var buttons = radio.find('li.table-view-cell > a');
+
+    if (options[radio.data('name')]) {
+      var button = buttons.filterByData('value', options[radio.data('name')]);
+      button.addClass('active');
+    }
 
     buttons.click(function() {
       buttons.removeClass('active');
